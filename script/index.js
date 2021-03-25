688479511300c5107a555bc46d0699f1f0ff80e2
stevending1st <1139274654@qq.com>
2021-03-25 16:17:36 +0800

    add -88

---
+++ script/index.js
@@ -0,0 +1,17 @@
+// Get the URL of the article from command line parameters.
+const getThirdParam = () => {
+  return new Promise((resolve, reject) =>{
+    const thirdParam = process.argv[2];
+    if(typeof(thirdParam) === "undefined"){
+    reject(Err_DontGetThirdParam);
+    } else {
+    resolve(thirdParam);
+    }
+  });
+};
+
+getThirdParam().then{
+  (log)=>{
+    console.log(log);
+  }
+};
