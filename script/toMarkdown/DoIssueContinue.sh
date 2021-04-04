9efce4a554d0f61ac1765eb266e6ea0511887b9b
stevending1st <1139274654@qq.com>
2021-04-04 11:02:02 +0800

    init

--- script/toMarkdown/DoIssueContinue.sh
+++ script/toMarkdown/DoIssueContinue.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-if [ "$2" ]; then
-  if [[ "$1" =~ ^\[Auto\]* ]]; then
-    echo "Ok"
-  else
-    echo "This issue does not need to generate a markdown file." 1>&2
-    exit 1;
-  fi;
-else
-  echo "The description of the issue is empty." 1>&2
-  exit 1;
-fi;
+echo "hello" $0 $1 $2
+# if [ "$2" ]; then
+#   if [[ "$1" =~ ^\[Auto\]* ]]; then
+#     echo "Ok"
+#   else
+#     echo "This issue does not need to generate a markdown file." 1>&2
+#     exit 1;
+#   fi;
+# else
+#   echo "The description of the issue is empty." 1>&2
+#   exit 1;
+# fi;
